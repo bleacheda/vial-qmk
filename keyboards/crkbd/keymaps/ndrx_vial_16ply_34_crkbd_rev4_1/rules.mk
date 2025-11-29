c897220c3a2f8913c7238d31a6e27dee6bc7ed2f
A. Staicu <bleacheda@gmail.com>
2025-11-29 02:11:24 +0200

    keyboards/crkbd/keymaps/ndrx_vial_16ply_34_crkbd_rev4_1

---
+++ keyboards/crkbd/keymaps/ndrx_vial_16ply_34_crkbd_rev4_1/rules.mk
@@ -0,0 +1,8 @@
+# if both VIA and VIAL are enabled, VIAL will take precedence and only VIAL GUI will work
+VIA_ENABLE         = yes
+VIAL_ENABLE        = yes
+VIALRGB_ENABLE     = yes
+CAPS_WORD_ENABLE = yes
+COMBO_ENABLE = yes
+REPEAT_KEY_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
